80e6780203753d801b61b5af9c73bf55e9af49ea
alexandarp <alexandar.podobnik@gmail.com>
2019-05-16 12:15:55 -0400

    trim terraform

--- main.tf
+++ main.tf
@@ -1,20 +1,9 @@
-resource "google_kms_key_ring" "default" {
-  name     = "vault-key-ring"
-  project  = "${var.project_id}"
-  location = "${var.region}"
-}
-
-resource "google_kms_crypto_key" "default" {
-  name     = "vault-crypto-key"
-  key_ring = "${google_kms_key_ring.default.self_link}"
-}
-
 module "vault" {
   source         = "terraform-google-modules/vault/google"
   project_id     = "${var.project_id}"
   region         = "${var.region}"
-  kms_keyring    = "${google_kms_key_ring.default.name}"
-  kms_crypto_key = "${google_kms_crypto_key.default.name}"
+  kms_keyring    = "${var.kms_keyring}"
+  kms_crypto_key = "${var.kms_crypto_key}"
 
   storage_bucket_force_destroy = true
 }
