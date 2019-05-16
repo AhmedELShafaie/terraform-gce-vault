b762eb26c66ee05067758ff37a56153bd394cdc3
alexandarp <alexandar.podobnik@gmail.com>
2019-05-16 12:05:09 -0400

    update terraform

--- main.tf
+++ main.tf
@@ -1,11 +1,12 @@
 resource "google_kms_key_ring" "default" {
   name     = "vault-key-ring"
+  project  = "${var.project_id}"
   location = "${var.region}"
 }
 
-data "google_kms_crypto_key" "default" {
+resource "google_kms_crypto_key" "default" {
   name     = "vault-crypto-key"
-  key_ring = "${data.google_kms_key_ring.default.self_link}"
+  key_ring = "${google_kms_key_ring.default.self_link}"
 }
 
 module "vault" {
@@ -13,7 +14,7 @@ module "vault" {
   project_id     = "${var.project_id}"
   region         = "${var.region}"
   kms_keyring    = "${google_kms_key_ring.default.name}"
-  kms_crypto_key = "${data.google_kms_crypto_key.name}"
+  kms_crypto_key = "${google_kms_crypto_key.default.name}"
 
   storage_bucket_force_destroy = true
 }
--- scripts/configure.sh
+++ scripts/configure.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
+export VAULT_ADDR="$(terraform output vault_addr)"
+export VAULT_CACERT="$(pwd)/ca.crt"
+
 vault operator init -recovery-shares 5 -recovery-threshold 3
